2ca641298354e5bf4bac56146fd52eacefc23d04
ariel <arielgimenez_95@hotmail.com>
2020-11-02 18:44:29 -0300

    haciendo que los numeros se acomoden automaticamente

--- Assets/scripts/InstanciarNumeros.cs
+++ Assets/scripts/InstanciarNumeros.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class InstanciarNumeros : MonoBehaviour
 {
-    public GameObject[] numeros;
+    public GameObject[] numeros;//cargo todos los gameObject numeros
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int x = 1; x < 6; x++)
+        {
+            GameObject numero = Instantiate(numeros[0],new Vector3(x,0,0),Quaternion.identity);//la posicion donde se instanciara
+            numero.transform.parent = this.gameObject.transform;//los numeros se instancian como hijos del Gameobjet que posee este script
+        }
     }
 
     // Update is called once per frame
