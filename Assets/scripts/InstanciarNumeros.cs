b7e9a1214872a462a07a022f4e1c4b680367ad62
ariel <arielgimenez_95@hotmail.com>
2020-11-03 17:56:11 -0300

    assets 2D y los números se acomodan automaticamente de forma aleatoria

--- Assets/scripts/InstanciarNumeros.cs
+++ Assets/scripts/InstanciarNumeros.cs
@@ -9,11 +9,27 @@ public class InstanciarNumeros : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int x = 1; x < 6; x++)
+        //este es un doble bucle para posicionar en el eje Y y X
+        for (int y = 0; y < 6; y++)//repite seis veces en el eje Y
         {
-            GameObject numero = Instantiate(numeros[0],new Vector3(x,0,0),Quaternion.identity);//la posicion donde se instanciara
-            numero.transform.parent = this.gameObject.transform;//los numeros se instancian como hijos del Gameobjet que posee este script
+            for (int x = 0; x < 6; x++)//repite seis veces en el eje X
+            {
+                int indiceAleatorioDeNumeros = Random.Range(0,numeros.Length);//obtiene un indice aleatorio desde cero hasta la cantidad de objetos del arreglo de números
+                //instancia el número de forma aleatoria en una posición
+                GameObject numero = Instantiate(numeros[indiceAleatorioDeNumeros],new Vector3
+                        (
+                            (1.25f+(-0.5f*x)),//acomoda en el eje X
+                            (1.25f+(-0.5f*y)),//Acomoda en el eje y
+                            0
+                        ),
+                        Quaternion.identity
+                    );//la posicion donde se instanciara
+                
+                numero.transform.parent = this.gameObject.transform;//los numeros se instancian como hijos del Gameobjet que posee este script (G)
+                print(indiceAleatorioDeNumeros);
+            }
         }
+        
     }
 
     // Update is called once per frame
