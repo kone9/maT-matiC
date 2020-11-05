2744239a70bf91d0edce069e98312662ed5286d8
ariel <arielgimenez_95@hotmail.com>
2020-11-05 19:19:30 -0300

    interface en UI y botones funcionando más animación en perosonaje con Musica

---
+++ Assets/scripts/Cronometro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cronometro : MonoBehaviour
+{
+    public int cronometro = 30;//Tiempo que se usara para el cronometro
+    private Text tiempo;//VAriable para obtener el text que se ve en pantalla
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiempo = GetComponent<Text>();//obntengo el componente
+        StartCoroutine("CuentaRegresiva");///llamo a la corrutina que hace la cuenta regresiva
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator CuentaRegresiva()//corrutina que cuenta treinta segundos
+    {
+        for (int i = 0; i < 30; i++) //El cronometro cuenta hasta treinta
+        {
+            yield return new WaitForSeconds(1f);//espero un segundo
+            cronometro -= 1;//descuento un valor al numero de cronometro
+            tiempo.text = Convert.ToString(cronometro);//uso el valor del número que es un int en el texto..convierto a String
+            
+        }
+        
+    }
+}
--- Assets/scripts/InstanciarNumeros.cs
+++ Assets/scripts/InstanciarNumeros.cs
@@ -1,32 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InstanciarNumeros : MonoBehaviour
 {
     public GameObject[] numeros;//cargo todos los gameObject numeros
-    
+    private GameObject canvas;
     // Start is called before the first frame update
     void Start()
     {
-        //este es un doble bucle para posicionar en el eje Y y X
+        canvas = GameObject.FindGameObjectWithTag("canvas");//busco el objeto que tiene etiqueta canvas importante para hacer hijos a los números instanciados
+
+        //Este es un doble bucle para posicionar en el eje Y y X
         for (int y = 0; y < 6; y++)//repite seis veces en el eje Y
         {
             for (int x = 0; x < 6; x++)//repite seis veces en el eje X
             {
                 int indiceAleatorioDeNumeros = Random.Range(0,numeros.Length);//obtiene un indice aleatorio desde cero hasta la cantidad de objetos del arreglo de números
                 //instancia el número de forma aleatoria en una posición
-                GameObject numero = Instantiate(numeros[indiceAleatorioDeNumeros],new Vector3
-                        (
-                            (-2.5f+(1f*x)),//acomoda en el eje X
-                            (-2.5f+(1f*y - 1)),//Acomoda en el eje y
-                            0
-                        ),
-                        Quaternion.identity
-                    );//la posicion donde se instanciara
+                GameObject numero = Instantiate(numeros[indiceAleatorioDeNumeros],new Vector3(0,0,0),Quaternion.identity);//Instancio el gameObject numero y lo guardo en una variable
                 
-                numero.transform.parent = this.gameObject.transform;//los numeros se instancian como hijos del Gameobjet que posee este script (G)
-                print(indiceAleatorioDeNumeros);
+                numero.gameObject.transform.SetParent(canvas.gameObject.transform);//hago que todos los números sean hijos de canvas
+                
+                //Como son botones tengo que buscarcar el componente rectTransform y posicionarlos en su localPosicion
+                numero.GetComponent<RectTransform>().localPosition= new Vector3
+                    (
+                        -100+(40*x),//acomoda en el eje X
+                        60-(40*y),//Acomoda en el eje y
+                        0
+                    );
+                numero.GetComponent<RectTransform>().localScale = new Vector3(1.2f,1.2f,0);//para que todos los botones tengan el mismo tamaño
+                
+                print(numero.GetComponent<RectTransform>().localPosition);
             }
         }
         
